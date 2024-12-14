53a614981288f7d84e3226264a31a36a1895e007
Mendez <conarov@gmail.com>
2024-12-15 01:18:28 +0200

    Initial Commit

---
+++ SwiftGithub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftGithub
+//
+//  Created by Mendez on 12/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftGithub/SwiftGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGithubApp.swift
+//  SwiftGithub
+//
+//  Created by Mendez on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
