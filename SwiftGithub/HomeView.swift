34dcd9ef8b6f059d50180446cbf1898c001ff188
Mendez <conarov@gmail.com>
2024-12-15 03:22:16 +0200

    HomeView Finish

--- SwiftGithub/HomeView.swift
+++ SwiftGithub/HomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
+        VStack {
             Text("Hello, World!")
             Text("SwiftGithub")
             Text("testing")
