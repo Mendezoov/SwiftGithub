2d2b095991c0b637c6cb33723cf56d4cc6553d0b
Mendez <conarov@gmail.com>
2024-12-15 03:20:06 +0200

    Add HomeView

---
+++ SwiftGithub/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftGithub
+//
+//  Created by Mendez on 12/15/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
