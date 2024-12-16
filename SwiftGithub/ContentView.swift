8f13086e4693739f68285f0ad44284250e80a7ec
Mendez <conarov@gmail.com>
2024-12-17 01:17:22 +0200

    [Feature] fix text with test from pulling the data

--- SwiftGithub/ContentView.swift
+++ SwiftGithub/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Mendez Development")
             Text("SwiftGithub")
+            Text("test")
             Button("Click me now ")
             {
                 
