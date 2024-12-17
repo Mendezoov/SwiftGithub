7b856198451907656ba5fc94d780978eca9d28a3
Mendez <conarov@gmail.com>
2024-12-17 02:01:34 +0200

    [patch] fix button

--- SwiftGithub/HomeView.swift
+++ SwiftGithub/HomeView.swift
@@ -13,7 +13,7 @@ struct HomeView: View {
         VStack {
             Text("Hi, World! stage all changes ")
             Text("SwiftGithub")
-            Text("test branch")
+            Text("fixed")
         }
     }
 }
