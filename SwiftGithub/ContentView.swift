ccd929ac22a09a9fdde42dd28b1c7e2a0276ac9a
Mendez <conarov@gmail.com>
2024-12-20 02:28:29 +0200

    [Clean] resolve merge conflicts

--- SwiftGithub/ContentView.swift
+++ SwiftGithub/ContentView.swift
@@ -11,25 +11,26 @@ struct ContentView: View {
     var body: some View {
         VStack {
             ScrollView {
-              VStack {
-                  ForEach(0..<20) { _ in
-                           Image(systemName: "house.fill")
-                .font(.largeTitle)
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Mendez Development")
-            Text("SwiftGithub")
-            Text("swift with second branch changes")
-            Button("nclick me ! ")
-            {
-                
-            }
+                VStack {
+                    ForEach(0..<20) { _ in
+                        Image(systemName: "house.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                            .foregroundStyle(.tint)
+                        Text("Mendez Development")
+                        Text("SwiftGithub")
+                        Text("swift with second branch changes")
+                        Button("nclick me ! ")
+                        {
+                            
+                        }
                     }
                 }
-         
-         
+                
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
